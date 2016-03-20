dcd242fc1c650778c5eba0fd8834be2262c79b96
dmitriy <dmitriy@dmitriyG-hp>
2016-03-20 15:57:33 +0200

    Adding methods to interact with products in DB.

--- src/java/dao/DatabaseInteraction.java
+++ src/java/dao/DatabaseInteraction.java
@@ -1,10 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package dao;
 
+import java.util.List;
 import org.hibernate.Session;
 
 /**
@@ -52,8 +48,18 @@ public class DatabaseInteraction {
         return true;
     }
 
-    public static Product showProduct() {
-
+    public static List getProductList(){
+        
+        List products;
+        
+        try (Session session = HibernateUtil.getSessionFactory().openSession()){
+            String queryString = "from Product";
+            products = session.createQuery(queryString).list();
+            return products;
+        }
+    }
+    
+    public static Product getProduct(Integer id){
         Product product;
         
         try (Session session = HibernateUtil.getSessionFactory().openSession()) {
