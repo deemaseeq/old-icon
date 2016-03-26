43208f4b0ae82ade33a816d4b7a8b186d615d061
deemaseeq <dmitriy.german38@outlook.com>
2016-03-26 20:35:52 +0200

    Adding a reaction for specific server responses.

--- web/js/signIn.js
+++ web/js/signIn.js
@@ -2,7 +2,13 @@ $(document).on("submit", "#signIn", function(event) {
     var $form = $(this);
 
     $.post($form.attr("action"), $form.serialize(), function(response) {
-        $("#errorMessage").text(response);
+        response = response.trim();
+        if(response === 'Succesful login.'){
+            location.reload();
+        } else {
+            $("#errorMessage").text(response);
+        }
+        
     });
 
     event.preventDefault(); // Important! Prevents submitting the form.
